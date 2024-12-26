f8da03a9193803d8eafcc778eebc140c8e7eb3c7
Super-123-Super <chideradokoye@gmail.com>
2024-12-26 22:35:32 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Hello")
+name = input("Enter Name: ")
+print("your name is "+ name)
+answer = input("what is one plus one: ") 
+if (answer == "2"):
+    print("yes")
+else:
+    print("nope")
