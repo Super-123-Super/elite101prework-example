ede1959c6a818bcbe93f4fe133907ad012e9035a
Super-123-Super <chideradokoye@gmail.com>
2025-01-18 02:00:37 +0000

    more updates

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-print("Hello")
+print("Hello and Welcome to the Chatbox")
 name = input("Enter Name: ")
-print("your name is "+ name)
-answer = input("what is one plus one: ") 
-if (answer == "2"):
-    print("yes")
+age = input("Enter age: ")
+print("your name is "+ name + " and you are "+ age + " years old")
+print("want to to math? lets do it!")
+difficulty = input("do you want easy or hard? ")
+if (difficulty == "easy"):
+    answer = input("what is one plus one: ") 
+    if (answer == "2"):
+        print("yes")
+    else:
+        print("aw man you lost on easy")
+    print("-------")
 else:
-    print("nope")
-print("-------")
-print("alright")
+    answer2 = input("what is 6 times 4: ")
+    if (answer2 == "24"):
+        print("yes")
+        print("wow you are good at math!")
+    else:
+        print("nope")
+    print("-------")
+    print("Good by " +name+ " and have a good day!!")
